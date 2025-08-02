3410292e7143df6799493d2104a63c6a179b9c33
kasstmaster <kassandraraudman@gmail.com>
2025-08-02 13:08:25 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAEd0LK81ubRrEn9vyg75nDFhb9MdcWnjw",
   authDomain: "rdmn-stream.firebaseapp.com",
   projectId: "rdmn-stream",
-  storageBucket: "rdmn-stream.firebasestorage.app",
+  storageBucket: "rdmn-stream.appspot.com",
   messagingSenderId: "748872080882",
   appId: "1:748872080882:web:ad167a25ebca9131025f6b"
 };
